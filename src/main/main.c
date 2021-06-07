81d254bf14bfec15557e7c7beb90a0ff2c8c45ae
Victor Dalacôrte <victor.dalacorte@ipcorp.com.br>
2021-06-06 23:04:09 -0300

    adding a simple background

--- src/main/main.c
+++ src/main/main.c
@@ -1,5 +1,7 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "background-map.c"
+#include "background-sprites.c"
 #include "pikachu-sprites.c"
 
 void main()
@@ -9,8 +11,15 @@ void main()
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
 
+    set_bkg_data(0, 7, backgroundTiles);
+    set_bkg_tiles(0, 0, 40, 18, backgroundMap);
+    SHOW_BKG;
+    DISPLAY_ON;
+
     while (1)
     {
+        scroll_bkg(1, 0);
+
         switch (joypad())
         {
         case J_LEFT:
