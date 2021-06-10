10546ab3f2f4d800179d2320916645b4f40e212e
Victor Dalacôrte <victor.dalacorte@ipcorp.com.br>
2021-06-09 21:54:11 -0300

    adding sound

--- src/main/main.c
+++ src/main/main.c
@@ -16,8 +16,25 @@ void main()
     SHOW_BKG;
     DISPLAY_ON;
 
+    NR52_REG = 0x80;
+    NR50_REG = 0x77;
+    NR51_REG = 0xFF;
+
     while (1)
     {
+
+        UBYTE joypad_state = joypad();
+
+        if (joypad_state)
+        {
+            NR10_REG = 0x16;
+            NR11_REG = 0x40;
+            NR12_REG = 0x73;
+            NR13_REG = 0x00;
+            NR14_REG = 0xC3;
+            delay(1000);
+        }
+
         scroll_bkg(1, 0);
 
         switch (joypad())
