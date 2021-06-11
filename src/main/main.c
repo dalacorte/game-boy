d39ec577cb096f94fead3082d952bc3e20e97897
Victor Dalacôrte <victor.dalacorte@ipcorp.com.br>
2021-06-11 17:00:07 -0300

    refactoring

--- src/main/main.c
+++ src/main/main.c
@@ -16,13 +16,13 @@ void main()
     SHOW_BKG;
     DISPLAY_ON;
 
-    NR52_REG = 0x80;
-    NR50_REG = 0x77;
-    NR51_REG = 0xFF;
+    // specific order
+    NR52_REG = 0x80; // 1000 0000 -> turns on sound
+    NR50_REG = 0x77; // set the volume for left and right channel
+    NR51_REG = 0xFF; // 1111 1111 -> select all chanels
 
     while (1)
     {
-
         UBYTE joypad_state = joypad();
 
         if (joypad_state)
@@ -32,7 +32,8 @@ void main()
             NR12_REG = 0x73;
             NR13_REG = 0x00;
             NR14_REG = 0xC3;
-            delay(1000);
+
+            delay(100);
         }
 
         scroll_bkg(1, 0);
