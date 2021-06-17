7d2de508bc1a1303831c41ccb2a8151efdf8f220
Victor Dalacôrte <victor.dalacorte@ipcorp.com.br>
2021-06-16 21:10:11 -0300

    jump

--- src/main/main.c
+++ src/main/main.c
@@ -6,6 +6,58 @@
 #include "window-map.c"
 #include "pikachu-sprites.c"
 
+INT8 playerLocation[2]; // x y
+BYTE jumping;
+UINT8 gravity = -2;
+UINT8 currentSpeedY;
+UINT8 floorY = 78;
+
+void perfomantDelay(UINT8 numLoops)
+{
+    UINT8 i;
+    for (i = 0; i < numLoops; i++)
+    {
+        wait_vbl_done();
+    }
+}
+
+INT8 wouldHitSurface(UINT8 projectedY)
+{
+    if (projectedY >= floorY)
+    {
+        return floorY;
+    }
+    return -1;
+}
+
+void jump()
+{
+
+    INT8 possibleY;
+
+    if (jumping == 0)
+    {
+        jumping = 1;
+        currentSpeedY = 10;
+    }
+
+    currentSpeedY = currentSpeedY + gravity;
+
+    playerLocation[1] = playerLocation[1] - currentSpeedY;
+
+    possibleY = wouldHitSurface(playerLocation[1]);
+
+    if (possibleY > -1)
+    {
+        jumping = 0;
+        move_sprite(0, playerLocation[0], possibleY);
+    }
+    else
+    {
+        move_sprite(0, playerLocation[0], playerLocation[1]);
+    }
+}
+
 void main()
 {
     font_t min_font;
@@ -19,7 +71,10 @@ void main()
 
     set_sprite_data(0, 2, pikachu);
     set_sprite_tile(0, 0);
-    move_sprite(0, 88, 78);
+    playerLocation[0] = 88;
+    playerLocation[1] = floorY;
+    jumping = 0;
+    move_sprite(0, playerLocation[0], playerLocation[1]);
     SHOW_SPRITES;
 
     set_bkg_data(37, 7, backgroundTiles); // starts 37
@@ -44,19 +99,24 @@ void main()
             NR12_REG = 0x73;
             NR13_REG = 0x00;
             NR14_REG = 0xC3;
-
-            delay(100);
         }
 
         scroll_bkg(1, 0);
 
+        if ((joypad() & J_A) || jumping == 1)
+        {
+            jump();
+        }
+
         switch (joypad())
         {
         case J_LEFT:
-            scroll_sprite(0, -10, 0);
+            playerLocation[0] = playerLocation[0] - 10;
+            move_sprite(0, playerLocation[0], playerLocation[1]);
             break;
         case J_RIGHT:
-            scroll_sprite(0, 10, 0);
+            playerLocation[0] = playerLocation[0] + 10;
+            move_sprite(0, playerLocation[0], playerLocation[1]);
             break;
         case J_UP:
             scroll_sprite(0, 0, -10);
@@ -66,6 +126,6 @@ void main()
             break;
         }
 
-        delay(100);
+        perfomantDelay(5);
     }
 }
